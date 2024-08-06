2f0f311b4ce0cb4f4f1b12b48edd6f89f9346add
basil204 <96483932+basil204@users.noreply.github.com>
2024-08-06 21:47:49 +0700

    fix lai security

--- src/main/java/com/example/thuthujava6_1/security/ConfigSecurity.java
+++ src/main/java/com/example/thuthujava6_1/security/ConfigSecurity.java
@@ -1,28 +1,50 @@
 package com.example.thuthujava6_1.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ConfigSecurity {
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Tạo người dùng với tên đăng nhập "admin", mật khẩu "admin" (đã được mã hóa) và vai trò "USER"
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin")) // Mã hóa mật khẩu
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Sử dụng BCrypt để mã hóa mật khẩu
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http    .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/rest/**").permitAll()
-                                .anyRequest().authenticated()
-                );
+        http
+
+                .authorizeRequests()
+                .anyRequest().authenticated() // Yêu cầu xác thực cho tất cả các yêu cầu
+                .and(); // Sử dụng xác thực HTTP Basic
+
         return http.build();
     }
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().requestMatchers(HttpMethod.OPTIONS,"/**");
-    }
+
 }
